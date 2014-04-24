808c5655a7475ca5f55b118dcd7a620a2d820edb
Matt Collins <mcollins@sovrn.net>
2014-04-24 12:01:00 -0600

    testing

--- tasks/rev.js
+++ tasks/rev.js
@@ -6,23 +6,15 @@ var path = require('path');
 module.exports = function(grunt) {
     grunt.registerMultiTask('rev_sovrn', 'Prefix static asset file names with a version number', function() {
         grunt.log.write("starting rev sovrn \n");
-
-        for(var key in this){
-            grunt.log.write(key + " \n");
-        };
-        
+        var version =  fs.readFile(this.files.versionSource);
         this.files.forEach(function(filePair) {
             filePair.src.forEach(function(f) {
-                var renamed = getVersion() + "." + path.basename(f);
+                var renamed = version + "." + path.basename(f);
                 var outPath = path.resolve(path.dirname(f), renamed);
 
                 fs.renameSync(f, outPath);
                 grunt.log.write(f + ' ').ok(renamed);
             });
         });
-        
-        function getVersion(){
-            return fs.readFile(this.files.versionSource);
-        }
     });
 };
