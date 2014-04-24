59e36dfd6ffb0655f85c44ceff76fdccef264c8c
Matt Collins <mcollins@sovrn.net>
2014-04-24 14:44:01 -0600

    testing

--- tasks/rev.js
+++ tasks/rev.js
@@ -5,22 +5,34 @@ var path = require('path');
 
 module.exports = function(grunt) {
     grunt.registerMultiTask('rev_sovrn', 'Prefix static asset file names with a version number', function() {
+        var fsOptions = {encoding: "utf8"};
         var options = grunt.task.current.options({'private': true});
-        var version = fs.readFileSync(options.versionSource, {encoding:"utf8"}).trim();
-        var htmlFile = fs.readFileSync(options.htmlFile, {encoding:"utf8"});
+        var version = JSON.parse(fs.readFileSync(options.versionSource, fsOptions));
+        var version_num = version.release + "_rc" + version.version;
+        var replaceFiles = options.replaceFiles;
         var pattern = options.pattern;
         
         this.files.forEach(function(filePair) {
             filePair.src.forEach(function(f) {
-                var renamed = version + "." + path.basename(f);
+                var renamed = version_num + "." + path.basename(f);
                 var outPath = path.resolve(path.dirname(f), renamed);
                 fs.renameSync(f, outPath);
                 grunt.log.write(f + ' ').ok(renamed);
             });
         });
         
-        htmlFile.replace(pattern, version);
-        fs.writeFileSync(options.htmlFile, htmlFile, {encoding:"utf8"});
-        grunt.log.write("Replaced values in HTML file.\n");
+        for(file in replaceFiles){
+            var contents = fs.readFileSync(file, fsOptions);
+            contents.replace(pattern, version_num);
+            fs.writeFileSync(file, contents, fsOptions);
+            grunt.log.write("Updated version in file: " + file + "\n");
+        }
+        
+        version.version++;
+        fs.writeFileSync(options.versionSource, JSON.stringify(version));
+        grunt.log.write("Updated the version number in version config file.\n ");
+        
+        
+        
     });
 };
