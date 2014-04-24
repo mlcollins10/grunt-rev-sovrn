0c2221a1b53a851196bafe2acb7f281261eeb8e5
Matt Collins <mcollins@sovrn.net>
2014-04-24 11:22:57 -0600

    created a rev task that will read a version number from a file

--- tasks/rev.js
+++ tasks/rev.js
@@ -1,49 +1,22 @@
-/*
- * grunt-rev
- * https://github.com/cbas/grunt-rev
- *
- * Copyright (c) 2013 Sebastiaan Deckers
- * Licensed under the MIT license.
- */
-
 'use strict';
 
-var fs = require('fs'),
-  path = require('path'),
-  crypto = require('crypto');
+var fs = require('fs');
+var path = require('path');
 
 module.exports = function(grunt) {
+    grunt.registerMultiTask('rev_sovrn', 'Prefix static asset file names with a version number', function() {
+        this.files.forEach(function(filePair) {
+            filePair.src.forEach(function(f) {
+                var renamed = getVersion() + "." + path.basename(f);
+                var outPath = path.resolve(path.dirname(f), renamed);
 
-  function md5(filepath, algorithm, encoding, fileEncoding) {
-    var hash = crypto.createHash(algorithm);
-    grunt.log.verbose.write('Hashing ' + filepath + '...');
-    hash.update(grunt.file.read(filepath), fileEncoding);
-    return hash.digest(encoding);
-  }
-
-  grunt.registerMultiTask('rev', 'Prefix static asset file names with a content hash', function() {
-
-    var options = this.options({
-      encoding: 'utf8',
-      algorithm: 'md5',
-      length: 8
+                fs.renameSync(f, outPath);
+                grunt.log.write(f + ' ').ok(renamed);
+            });
+        });
+        
+        function getVersion(){
+            return fs.readFile(this.files.versionSource);
+        }
     });
-
-    this.files.forEach(function(filePair) {
-      filePair.src.forEach(function(f) {
-
-        var hash = md5(f, options.algorithm, 'hex', options.encoding),
-          prefix = hash.slice(0, options.length),
-          renamed = [prefix, path.basename(f)].join('.'),
-          outPath = path.resolve(path.dirname(f), renamed);
-
-        grunt.verbose.ok().ok(hash);
-        fs.renameSync(f, outPath);
-        grunt.log.write(f + ' ').ok(renamed);
-
-      });
-    });
-
-  });
-
 };
